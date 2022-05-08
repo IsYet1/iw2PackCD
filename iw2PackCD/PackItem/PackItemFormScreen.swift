fb23d380be68af1f1442d2aaa725fd0f8010bead
IsYet1 <Don@IsYet.com>
2022-05-08 17:26:07 -0500

    Category pick list is displaying options.

--- iw2PackCD/PackItem/PackItemFormScreen.swift
+++ iw2PackCD/PackItem/PackItemFormScreen.swift
@@ -10,32 +10,35 @@ import SwiftUI
 struct PackItemFormScreen: View {
     @Environment(\.presentationMode) var presentationMode
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Category.name, ascending: true)],
-        animation: .default)
+        sortDescriptors: [NSSortDescriptor(keyPath: \Category.name, ascending: true)]
+    )
     private var categories: FetchedResults<Category>
     
     @StateObject private var formVM = PackItemFormViewModel()
+    @State private var selectedCategory = "Test"
     
     var body: some View {
         VStack {
             Text("Add a Pack Item")
                 .font(.title)
-            Form {
-                TextField("Pack Item Name", text: $formVM.name)
-                    .padding(.all, 10.0)
-                
-                HStack {
-                    Button("Save") {
-                        formVM.save()
-                        presentationMode.wrappedValue.dismiss()
-                    }.padding()
-                    Button("Cancel") {
-                        // TODO: This shouldn't save blank items.
-                        presentationMode.wrappedValue.dismiss()
-                    }.padding()
+            TextField("Pack Item Name", text: $formVM.name)
+                .padding(.all, 10.0)
+            Picker("Category", selection: $selectedCategory) {
+                ForEach(categories, id: \.self) {(category ) in
+                    Text(category.name!)
                 }
-                .padding(.vertical)
             }
+            HStack {
+                Button("Save") {
+                    formVM.save()
+                    presentationMode.wrappedValue.dismiss()
+                }.padding()
+                Button("Cancel") {
+                    // TODO: This shouldn't save blank items.
+                    presentationMode.wrappedValue.dismiss()
+                }.padding()
+            }
+            .padding(.vertical)
         }
     }
 }
@@ -45,3 +48,4 @@ struct PackItemFormScreen_Previews: PreviewProvider {
         PackItemFormScreen()
     }
 }
+
