5e2d988e88a0c76c09d75d0c26882ed5747d8260
DonMck <donmck@amazon.com>
2022-06-09 18:00:52 -0500

    Add event items DB and relationships

--- iw2PackCD/StartView.swift
+++ iw2PackCD/StartView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct StartView: View {
     var body: some View {
         TabView {
+            EventListScreen().tabItem() {Text("Events")}
             PackItemListScreen().tabItem() {Text("Pack Items VM")}
             CategoryList().tabItem() {Text("Category")}
 //            LocationList().tabItem() {Text("Location")}
-            EventListScreen().tabItem() {Text("Events")}
         }
     }
 }
