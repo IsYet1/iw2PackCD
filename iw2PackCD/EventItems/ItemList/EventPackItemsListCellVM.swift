dc1a9f0534eeb00bcd9d1cb47bcee019958fc8c8
DonMck <donmck@amazon.com>
2022-07-12 10:02:39 -0500

    Handle item deleted in EventItems listing Work around. Probably will implement a better fix later.

--- iw2PackCD/EventItems/ItemList/EventPackItemsListCellVM.swift
+++ iw2PackCD/EventItems/ItemList/EventPackItemsListCellVM.swift
@@ -20,8 +20,15 @@ class EventItemListCellVM: ObservableObject {
         itemPacked = eventItemIn.packed
         
         // TODO: Add error check and handle here.
-        packItemName = eventItemIn.item!.name!
-        packItemCategory = eventItemIn.item!.categoryName
+        if let itemIn = eventItemIn.item {
+            packItemName = itemIn.name ?? "Item name not set"
+            packItemCategory = itemIn.categoryName
+        } else {
+            packItemName = "Item Not Found"
+            packItemCategory = "Item Not Found"
+        }
+//        packItemName = eventItemIn.item?.name ?? "Item Not Found"
+//        packItemCategory = eventItemIn.item!.categoryName
     }
     
     func updatePackedStatus(packed: Bool) {
