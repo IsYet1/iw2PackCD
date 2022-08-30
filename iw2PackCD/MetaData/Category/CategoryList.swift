1b19bdb5c5867015981fb4936c23d76a85f8d665
donmck <donmck@amazon.com>
2022-08-29 19:02:26 -0500

    Basic is working. Unsure about setting the width for the sidebar.

--- iw2PackCD/MetaData/Category/CategoryList.swift
+++ iw2PackCD/MetaData/Category/CategoryList.swift
@@ -11,6 +11,7 @@ struct CategoryList: View {
     
     @Environment(\.managedObjectContext) private var viewContext
     @State private var showForm: Bool = false
+    @State private var selectedCategory: Category? = nil
     
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Category.name, ascending: true)],
@@ -18,29 +19,45 @@ struct CategoryList: View {
     private var items: FetchedResults<Category>
     
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    CategoryItemCell(item: item)
+        NavigationSplitView(
+            sidebar: {
+                List(items, id: \.self.id,
+                     selection: $selectedCategory,
+                     rowContent: {category in NavigationLink(category.name!, value: category)}
+                )
+                
+            },
+            detail: {
+                if (selectedCategory != nil) {
+                    CategoryPackItemsList(category: selectedCategory!)
+                } else {
+                    Text("Select a category")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .navigationTitle("Categories")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    EditButton()
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("Add") {
-                        self.showForm = true
-                    }
-                }
-            }
-            .sheet(isPresented: $showForm, content: {
-                CategoryFormScreen()
             })
-            Text("Select an item")
-        }
+        
+//        NavigationView {
+//            List {
+//                ForEach(items) { item in
+//                    CategoryItemCell(item: item)
+//                }
+//                .onDelete(perform: deleteItems)
+//            }
+//            .navigationTitle("Categories")
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarLeading) {
+//                    EditButton()
+//                }
+//                ToolbarItem(placement: .navigationBarTrailing) {
+//                    Button("Add") {
+//                        self.showForm = true
+//                    }
+//                }
+//            }
+//            .sheet(isPresented: $showForm, content: {
+//                CategoryFormScreen()
+//            })
+//            Text("Select an item")
+//        }
     }
     
     private func deleteItems(offsets: IndexSet) {
@@ -57,11 +74,11 @@ struct CategoryList: View {
     }
 }
 
-struct CategoryList_Previews: PreviewProvider {
-    static var previews: some View {
-        CategoryList()
-    }
-}
+//struct CategoryList_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CategoryList()
+//    }
+//}
 
 struct CategoryItemCell: View {
     let item: Category
